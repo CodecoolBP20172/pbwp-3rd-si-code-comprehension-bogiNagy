523d2ec33a92878e430df678c3f4c439a0721a64
Boglarka Nagy <thebogosz@gmail.com>
2017-07-01 17:54:37 +0200

    final comments

--- comprehension.py
+++ comprehension.py
@@ -1,33 +1,33 @@
 import random
-
+# it is a guessing game
 guessesTaken = 0
 
 print('Hello! What is your name?')
 myName = input()
-
-number = random.randint(1, 20)
+# name of the user
+number = random.randint(1, 20)  # it's generates a number between 1 and 20
 print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
 
-while guessesTaken < 6:
+while guessesTaken < 6:  # makes a limit, after it, this loop will end
     print('Take a guess.')
-    guess = input()
-    guess = int(guess)
+    guess = input()  # asks for an input
+    guess = int(guess)  # the input is now an integer(number) not a string
 
-    guessesTaken += 1
+    guessesTaken += 1 
 
-    if guess < number:
+    if guess < number:  # examines the guessed number if it's lower...
         print('Your guess is too low.')
 
-    if guess > number:
+    if guess > number:  # ...or higher than the right number
         print('Your guess is too high.')
 
-    if guess == number:
+    if guess == number:  # if the users guess is right
         break
 
-if guess == number:
-    guessesTaken = str(guessesTaken)
+if guess == number:  # if the user guessed the right number
+    guessesTaken = str(guessesTaken) 
     print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')
 
-if guess != number:
+if guess != number:  # if the user did not guessed the right number after 6 guesses
     number = str(number)
     print('Nope. The number I was thinking of was ' + number)
